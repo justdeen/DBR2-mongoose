f80191ad9382a2c2f90ab9846e6abc39a551190d
Sheriff Aliu Yamah <112347873+justdeen@users.noreply.github.com>
2025-01-08 09:46:19 +0100

    Restructured the Err obj.

--- AppError.js
+++ AppError.js
@@ -1,9 +1,3 @@
-class AppError extends Error{
-    constructor(message, status){
-        super();
-        this.message = message;
-        this.status = status
-    }
-}
+
 
 module.exports = AppError
