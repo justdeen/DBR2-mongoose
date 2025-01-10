f625cc242c7d788c1f4483d908c7c006387e9346
Sheriff Aliu Yamah <112347873+justdeen@users.noreply.github.com>
2025-01-09 19:27:32 -0800

    Added custom alerts

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
+const Product = require('./models/product')
 const methodOverride = require('method-override')
 const AppError = require('./AppError')
 
-const Product = require('./models/product')
-const Farm = require('./models/farm')
-const { name } = require('ejs')
+const categories = ['fruit', 'vegetable', 'dairy']
 
-mongoose.connect('mongodb://127.0.0.1:27017/farms')
+mongoose.connect('mongodb://127.0.0.1:27017/farmstand2')
 .then(() => {
     console.log('CONNECTION OPEN')
 })
@@ -24,63 +23,6 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
-const categories = ['fruit', 'vegetable', 'dairy']
-
-
-
-
-// FARM ROUTES
-
-app.get('/farms', async (req, res) => {
-    const farms = await Farm.find({})
-    res.render('farms/index', { farms })
-})
-
-app.get('/farms/new', (req, res) => {
-    res.render('farms/new')
-})
-
-app.get('/farms/:id', async  (req, res) => {
-    const farm = await Farm.findById(req.params.id).populate('products');
-    console.log(farm)
-    res.render('farms/show', { farm })
-})
-
-app.post('/farms', async (req, res) => {
-    const farm = new Farm(req.body);
-    await farm.save();
-    res.redirect('/farms')
-})
-
-app.get('/farms/:id/products/new', async (req, res) => {
-    const { id } = req.params;
-    const farm = await Farm.findById(id)
-    res.render('products/new', { categories, farm })
-})
-
-app.post('/farms/:id/products', async (req, res) => {
-    const { name, price, category } = req.body;
-    const { id } = req.params; 
-    const farm = await Farm.findById(id);
-    const product  = new Product({name, price, category});
-    farm.products.push(product);
-    product.farm = farm;
-    await farm.save();
-    await product.save();
-    res.redirect(`/farms/${id}`)
-})
-
-app.delete('/farms/:id', async (req, res) => {
-    const { id } = req.params
-    const farm = await Farm.findByIdAndDelete(id);
-    res.redirect('/farms');
-})
-
-
-
-
-
-// PRODUCTS ROUTES
 app.get('/products',  wrapAsync(async (req, res) => {
     const {category} = req.query
     if(category){
@@ -104,7 +46,7 @@ function wrapAsync(fn){  // 'fn' is equal to each of the async functions in each
 
 app.get('/products/:id', wrapAsync(async (req, res, next) => {
     const {id} = req.params
-    const product = await Product.findById(id).populate('farm')
+    const product = await Product.findById(id)
     res.render('products/show', {product})
 }))
 
